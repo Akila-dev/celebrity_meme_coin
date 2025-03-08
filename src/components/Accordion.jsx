57ed7dc25fc33ab5e31598410d58c52fcfe68614
unknown <akilapanunayan@gmail.com>
2025-03-08 07:17:54 +0100

    init

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 
 import { slideIn } from '../utils/motion';
+import { IoIosArrowDown } from 'react-icons/io';
 // import { fadeIn, textVariant } from '../utils/motion';
 
 const Accordion = ({ data }) => {
@@ -12,23 +13,36 @@ const Accordion = ({ data }) => {
 			{data.map((item, index) => (
 				<motion.div key={index} className="rounded-xl overflow-hidden">
 					<motion.button
-						className="rounded-xl relative !block w-full text-left p-[1em] lg:px-[2em]"
+						className="rounded-xl relative flex items-start justify-between gap-[2em] w-full text-left p-[1em] lg:px-[2em] "
 						onClick={() => setExpanded(index === expanded ? false : index)}
 						type="button"
 					>
 						{item.title}
+						<IoIosArrowDown
+							layout
+							style={{
+								transform:
+									index === expanded ? 'rotate(180deg)' : 'rotate(0deg)',
+							}}
+							transition={{
+								type: 'spring',
+								visualDuration: 5,
+								bounce: 0.2,
+							}}
+							className="scale-[110%] mt-[0.25em]"
+						/>
 					</motion.button>
 
 					{/* Dropdown content */}
 					<AnimatePresence initial={false}>
 						<motion.div
 							key="content"
+							style={{ height: index === expanded ? 'fit-content' : 0 }}
 							transition={{
 								type: 'spring',
-								visualDuration: 0.2,
+								visualDuration: 5,
 								bounce: 0.2,
 							}}
-							style={{ height: index === expanded ? 'fit-content' : 0 }}
 							layout
 							className="overflow-hidden"
 						>
